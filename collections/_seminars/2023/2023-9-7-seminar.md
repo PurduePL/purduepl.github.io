811e4bf28065e98d47d1bfe5710c09db032fdc0c
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2023-08-22 16:39:15 -0400

    script to be lazy, scraping the seminar schedule

---
+++ collections/_seminars/2023/2023-9-7-seminar.md
@@ -0,0 +1,12 @@
+---
+layout: post
+speaker: "Paschal Amusuo"
+title: "(ASE Practice) EmNetTest: Systematically Detecting Packet Processing Vulnerabilities in Embedded Network Stacks"
+time: 12p EST
+location: "LWSN 3102A/B"
+category: seminar
+invited: false
+link_abstract: true
+bio: "TBA"
+---
+TBA
---
+++ collections/_seminars/scrape_seminar_google_doc.py
@@ -0,0 +1,70 @@
+import gspread
+import json
+from dateutil.parser import *
+
+print(
+    "Warning, don't run a script from the internet that you haven't read before... certainly don't give it write permissions - Patrick, August 22, 2023"
+)
+
+gc = gspread.service_account()
+google_sheet_url = input()
+print(google_sheet_url)
+sh = gc.open_by_url(google_sheet_url)
+print(sh.title)
+print(sh.sheet1)
+
+records = sh.sheet1.get_all_records()
+
+print("Asserting some kind of format")
+for record in records:
+    assert "Date" in record.keys()
+    assert "Presenter" in record.keys()
+    assert "Title" in record.keys()
+    assert "Location" in record.keys()
+
+print("The unfiltered list of records")
+
+
+def date_filter(x):
+    try:
+        parse(x["Date"])
+        return True
+    except ValueError:
+        print("Date filtered out:\n", json.dumps(x, indent=2))
+        return False
+
+
+def presenter_filter(x):
+    result = x["Presenter"] != "" and x["Presenter"] != "N/A"
+    if not result:
+        print("Presenter filtered out:\n", json.dumps(x, indent=2))
+
+    return result
+
+
+print(json.dumps(records, indent=2))
+records = filter(date_filter, records)
+records = filter(presenter_filter, records)
+""" print(json.dumps(list(records), indent=2)) """
+for record in list(records):
+    date = parse(record["Date"])
+    presenter = record["Presenter"]
+    title = record["Title"]
+    location = record["Location"]
+    file_name = f"{date.year}/{date.year}-{date.month}-{date.day}-seminar.md"
+    file_contents = f"""---
+layout: post
+speaker: "{presenter}"
+title: "{title}"
+time: 12p EST
+location: "{location}"
+category: seminar
+invited: false
+link_abstract: true
+bio: "TBA"
+---
+TBA
+"""
+    print(file_name)
+    print(file_contents)
+    open(file_name, mode="w").write(file_contents)
