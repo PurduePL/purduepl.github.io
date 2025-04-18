a6f309a29f0497c722f620da0d980a14bb30aaca
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-04-18 11:52:45 -0400

    Create a little script to automate turning issues into posts

---
+++ collections/generate_post_from_issue.py
@@ -0,0 +1,60 @@
+import subprocess
+import json
+import shutil
+import os
+
+assert shutil.which("gh") is not None, "GitHub CLI (gh) is not installed. Please install it from https://cli.github.com/ and try again."
+
+issue_number = input("Enter the issue number: ")
+
+json_string = subprocess.run(["gh", "issue", "view", issue_number, "--json", "title,body"], check=True, capture_output=True).stdout
+
+data = json.loads(json_string)
+
+title = data["title"]
+
+if title.startswith("News: "):
+    title = title[6:]
+
+assert title != "", "Title not found in issue body. Please ensure the issue body contains a title in the format 'News: <title>' or '<title>'"
+
+data = data["body"].split("###")
+
+date = data[1].strip()
+
+assert date != "", "Date not found in issue body. Please ensure the issue body contains a date in the format '### DATE: YYYY-MM-DD'."
+
+if date.startswith("DATE"):
+    date = date[5:].strip()
+
+body = data[2].strip()
+
+if body.startswith("Content"):
+    body = body[8:].strip()
+
+assert body != "", "Body not found in issue body. Please ensure the issue body contains a body in the format '### Content: <body>' or '<body>'"
+
+print(f"Title: {title}")
+print(f"Date: {date}")
+print(f"Body: {body}")
+
+print("Generating post from issue...")
+
+filename = f"_posts/{date}-news.markdown"
+
+assert not os.path.exists(filename), f"File {filename} already exists. Please delete it or choose a different date."
+
+## make a post in _posts/{data}-news.markdown
+with open(filename, "w") as f:
+    f.write(f"---\n")
+    f.write(f"layout: post\n")
+    f.write(f"title: \"{title}\"\n")
+    f.write(f"category: news\n")
+    f.write(f"---\n\n")
+    f.write(f"{body}\n")
+
+subprocess.run(["git", "add", filename], check=True)
+
+subprocess.run(["git", "commit", "-m", f"Fix #{issue_number}"], check=True)
+
+subprocess.run(["git", "push"], check=True)
